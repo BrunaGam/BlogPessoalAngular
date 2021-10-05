f423dfdae70d31588c61bbaf43fbfa94f1842022
Bruna Amoretti Guimarães <brubru.amoretti@gmail.com>
2021-10-05 14:13:21 -0300

    CRUD completo da model: Tema | 05.10.2021

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,13 +11,33 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
-  constructor( private router: Router) { }
+  tema: Tema = new Tema ();
+  listaTemas: Tema[]
+
+
+  constructor( private router: Router, private temaService: TemaService) { }
 
   ngOnInit(){
     if(environment.token == ''){
       alert('Sua sessão expirou! Faça o login novamente.')
       this.router.navigate(['/login'])
       }
-  }
 
+      this.getAllTemas()
+     }
+
+     getAllTemas(){
+      this.temaService.getAllTemas().subscribe((resp: Tema[]) => {
+        this.listaTemas = resp
+        })
+     }
+
+  cadastrar(){
+  this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+  this.tema = resp
+  alert('Tema criado com sucesso!!!!!')
+  this.getAllTemas()
+  this.tema = new Tema()
+  })
+}
 }
